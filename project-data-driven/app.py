9443140f929769e1ce8a0c06fb229da97aba2fdd
Manuel <15770711+Narshe1412@users.noreply.github.com>
2019-07-31 03:32:55 +0100

    delete task functionality completed (#9)

--- project-data-driven/app.py
+++ project-data-driven/app.py
@@ -53,6 +53,12 @@ def update_task(task_id):
     return redirect(url_for('get_tasks'))
 
 
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({"_id": ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
+
+
 # Main
 if __name__ == "__main__":
     if(os.environ.get("WINDIR")):
