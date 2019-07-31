e87e45bc223d06c74002ffdf98677f1d911cc68b
Manuel <15770711+Narshe1412@users.noreply.github.com>
2019-07-31 04:00:22 +0100

    Categories (#10)
    * list categories in a new template for modification
    
    * edit category implemented
    
    * delete category implemented
    
    * add task implemented

--- project-data-driven/app.py
+++ project-data-driven/app.py
@@ -59,6 +59,43 @@ def delete_task(task_id):
     return redirect(url_for('get_tasks'))
 
 
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', categories=mongo.db.categories.find())
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editCategory.html',
+                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
+
+
 # Main
 if __name__ == "__main__":
     if(os.environ.get("WINDIR")):
